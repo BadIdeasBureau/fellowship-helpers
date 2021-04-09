975ff27de9efd0a12ea127fd22de0cde4f5be6aa
BadIdeasBureau <dumbmuscle@gmail.com>
2021-04-09 19:36:40 +0100

    Bugfix.  That's not how libWrapper works

--- scripts/module.js
+++ scripts/module.js
@@ -1,34 +1,36 @@
 Hooks.once('ready', async function() {
     libWrapper.register("fellowship-helpers","game.pbta.PbtaRolls.rollMoveExecute", async function (wrapped, ...args){
         const actor = this.actor;
+        console.log("Pre-dialog")
         //todo: handle vigor and drunk (and both)
-        const d = new Dialog({
+        const oldRoll = actor.data.data?.resources?.rollFormula?.value;
+        const newRoll = await new Promise((resolve) => new Dialog({
             title: "Roll",
             buttons:{
                 hope:{
                     icon: "<i class='fas fa-heart'></i>",
                     label: "Hope",
-                    callback: async () => await doRoll("3d6kh2")
+                    callback: () => resolve("3d6kh2")
                 },
                 normal:{
                     icon: "<i class='fa fa-circle'></i>",
                     label: "Normal",
-                    callback: async () => await doRoll("2d6")
+                    callback: () => resolve("2d6")
                 },
                 despair:{
                     icon: "<i class='fas fa-heart-broken'></i>",
                     label: "Despair",
-                    callback: async () => await doRoll("3d6kl2")
+                    callback: () => resolve("3d6kl2")
                 }
-            }
-        })
-        d.render(true);
-        //do dialog to grab new roll
-        async function doRoll(newRoll) {
-            const oldRoll = actor.data.data?.resources?.rollFormula?.value;
-            await actor.update({"data.resources.rollFormula.value": newRoll});
-            await wrapped(...args);
-            await actor.update({"data.resources.rollFormula.value": oldRoll})
+            },
+            close: () => resolve(undefined)
+        }).render(true));
+
+        if (newRoll) {
+            await actor.update({ "data.resources.rollFormula.value": newRoll });
+            const result = wrapped(...args);
+            await actor.update({ "data.resources.rollFormula.value": oldRoll });
+            return result;
         }
-    }, "MIXED")
+    }, "MIXED");
 });
