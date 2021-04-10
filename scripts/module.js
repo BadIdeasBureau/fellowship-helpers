a2b05bf9c5af686879c5ad8ba51b3d7bb7cd2adf
BadIdeasBureau <dumbmuscle@gmail.com>
2021-04-10 01:21:36 +0100

    Prompt should now affect stat rolls, and no longer shows for non-rolls

--- scripts/module.js
+++ scripts/module.js
@@ -1,9 +1,20 @@
+import {PbtaUtility} from "../../../../AppData/Local/FoundryVTT/Data/systems/pbta/module/utility";
+
 Hooks.once('ready', async function() {
     libWrapper.register("fellowship-helpers","game.pbta.PbtaRolls.rollMoveExecute", async function (wrapped, ...args){
+        let formula = args[0];
+        if (PbtaUtility.isEmpty(formula)) return await wrapped(...args); // if it's not a roll, don't ask for anything.
         const actor = this.actor;
-        console.log("Pre-dialog")
+        const oldRoll = actor.data.data?.resources?.rollFormula?.value ?? game.pbta.sheetConfig.rollFormula ?? "2d6";
+        let isStatRoll = false;
+        let parts;
+        if (formula instanceof String){ //if a formula is defined as a string, it wants to roll that - but could be a stat roll.  Only way to tell is if it's picked up the rollFormula.
+            parts = formula.split("+");
+            if (parts[0] === oldRoll){
+                isStatRoll = true;
+            }
+        }
         //todo: handle vigor and drunk (and both)
-        const oldRoll = actor.data.data?.resources?.rollFormula?.value ?? "2d6";
         const newRoll = await new Promise((resolve) => new Dialog({
             title: "Roll",
             buttons:{
@@ -27,10 +38,16 @@ Hooks.once('ready', async function() {
         }).render(true));
 
         if (newRoll) {
-            await actor.update({ "data.resources.rollFormula.value": newRoll });
-            const result = wrapped(...args);
-            await actor.update({ "data.resources.rollFormula.value": oldRoll });
-            return result;
+            if (isStatRoll && parts){
+                parts[0] = newRoll;
+                args[0] = parts.join("+");
+                return wrapped(...args);
+            } else {
+                await actor.update({"data.resources.rollFormula.value": newRoll});
+                const result = wrapped(...args);
+                await actor.update({"data.resources.rollFormula.value": oldRoll});
+                return result;
+            }
         }
     }, "MIXED");
 });
